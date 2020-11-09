76d2a32f1eefb68bf818a34c8957d5f39a2ee58c
Apple <apple@MacbookPro.local>
2020-11-09 12:01:30 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: "Montserrat"
+      },
+      colors: {
+        lightblack: '#333333'
+      }
+    },
+    maxWidth: {
+      maximg: "560px",
+      maximgsm: "435px" 
+    }
+  },
+  variants: {},
+  plugins: [],
+}
